ad2921d91a07238a4120574994cbb06eedfc31e1
faithckorir <korir.faith@eclectics.io>
2023-08-02 20:17:03 +0300

    added starter code

--- src/main/java/com/faithckorir/Main.java
+++ src/main/java/com/faithckorir/Main.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args){
+        //added starter code
         SpringApplication.run(Main.class, args);
     }
 }
